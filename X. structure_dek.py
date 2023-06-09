60461224d4fd6949ebdc6e6072c87b5b02602746
Alexman2505 <alexman2505@gmail.com>
2023-06-10 00:09:07 +0300

    первое задание на Дек готово на отправку

--- X. structure_dek.py
+++ X. structure_dek.py
@@ -1,58 +1,96 @@
+from typing import Tuple
+
+
+class InputError(Exception):
+    pass
+
+
 class Deque:
-    def __init__(self, size):
+    def __init__(self, size: int):
         self.size = size
         self.buffer = [None] * size
-        self.front = 0  # указатель начала дека
-        self.rear = 0  # указатель конца дека
+        self.head = 0  # указатель начала дека
+        self.tail = 0  # указатель конца дека
         self.count = 0  # текущее количество элементов в деке
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self.count == 0
 
-    def is_full(self):
+    def is_full(self) -> bool:
         return self.count == self.size
 
-    def push_front(self, value):
+    def push_front(self, value: int) -> None:
         if self.is_full():
             print("error")
         else:
-            self.front = (self.front - 1) % self.size
-            self.buffer[self.front] = value
+            self.head = (self.head - 1) % self.size
+            self.buffer[self.head] = value
             self.count += 1
 
-    def push_back(self, value):
+    def push_back(self, value: int) -> None:
         if self.is_full():
             print("error")
         else:
-            self.buffer[self.rear] = value
-            self.rear = (self.rear + 1) % self.size
+            self.buffer[self.tail] = value
+            self.tail = (self.tail + 1) % self.size
             self.count += 1
 
-    def pop_front(self):
+    def pop_front(self) -> None:
         if self.is_empty():
             print("error")
         else:
-            value = self.buffer[self.front]
-            self.front = (self.front + 1) % self.size
+            value = self.buffer[self.head]
+            self.head = (self.head + 1) % self.size
             self.count -= 1
             print(value)
 
-    def pop_back(self):
+    def pop_back(self) -> None:
         if self.is_empty():
             print("error")
         else:
-            self.rear = (self.rear - 1) % self.size
-            value = self.buffer[self.rear]
+            self.tail = (self.tail - 1) % self.size
+            value = self.buffer[self.tail]
             self.count -= 1
             print(value)
 
 
-def process_commands(n, m):
+def read_input() -> Tuple[int, int]:
+    try:
+        n = int(input())
+    except ValueError:
+        raise InputError(
+            'Ошибка! Было введено не число. Введите число в диапазоне от 1 до 100000\n'
+        )
+    if not 1 <= n <= 100000:
+        raise InputError(
+            'Ошибка! Число должно быть в диапазоне от 1 до 100000\n'
+        )
+    try:
+        m = int(input())
+    except ValueError:
+        raise InputError(
+            'Ошибка! Было введено не число. Введите число в диапазоне от 1 до 50000\n'
+        )
+    if not 1 <= m <= 50000:
+        raise InputError(
+            'Ошибка! Число должно быть в диапазоне от 1 до 50000\n'
+        )
+    return n, m
+
+
+def read_commands(n: int, m: int) -> None:
     deque = Deque(m)
 
     for _ in range(n):
         command = input().split()
-        if command[0] == "push_front":
+        if len(command) == 1 and (
+            command[0] == "push_front" or command[0] == "push_back"
+        ):
+            raise InputError(
+                'Ошибка! Команды "push_front" и "push_back"'
+                ' должны содержать числа\n'
+            )
+        elif command[0] == "push_front":
             deque.push_front(int(command[1]))
         elif command[0] == "push_back":
             deque.push_back(int(command[1]))
@@ -60,9 +98,13 @@ def process_commands(n, m):
             deque.pop_front()
         elif command[0] == "pop_back":
             deque.pop_back()
+        else:
+            raise InputError("Ошибка! Вы ввели недопустимый метод\n")
 
 
 if __name__ == "__main__":
-    n = int(input())
-    m = int(input())
-    process_commands(n, m)
+    try:
+        n, m = read_input()
+        read_commands(n, m)
+    except InputError as e:
+        print(e)
