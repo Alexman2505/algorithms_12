6aa603b92b711331bff121b6ded865953ab0f3e7
Alexman2505 <alexman2505@gmail.com>
2023-06-10 12:02:49 +0300

    переделал вторую функцию

---
+++ t_review_b_pol_calc.py
@@ -0,0 +1,66 @@
+class InputError(Exception):
+    pass
+
+
+def validate_expression(expression: str) -> str:
+    values_expression = expression.split()
+    if len(values_expression) <= 3 and values_expression[2] not in (
+        '+',
+        '-',
+        '*',
+        '/',
+    ):
+        raise InputError(
+            "Ошибка! Некорректные входные данные. "
+            "Введите минимум 2 числа и знак операции.\n"
+        )
+    if len(values_expression) % 2 == 0:
+        raise InputError(
+            "Ошибка! Некорректные входные данные. "
+            "Неправильное количество цифр и знаков операций.\n"
+        )
+
+    for operation in values_expression:
+        if operation not in ('+', '-', '*', '/'):
+            try:
+                operand = int(operation)
+                if abs(operand) > 10000:
+                    raise InputError(
+                        "Ошибка! Числа во входных данные должны быть "
+                        "по модулю не превосходящими 10000.\n"
+                    )
+            except ValueError:
+                raise InputError("Ошибка! Вы ввели не число.\n")
+    return expression
+
+
+def calculator(expression: str) -> int:
+    stack = []
+
+    for operation in expression.split():
+        if operation not in ('+', '-', '*', '/'):
+            stack.append(operation)
+        else:
+            b = int(stack.pop())
+            a = int(stack.pop())
+            if operation == '+':
+                stack.append(a + b)
+            if operation == '-':
+                stack.append(a - b)
+            if operation == '*':
+                stack.append(a * b)
+            if operation == '/':
+                if b == 0:
+                    raise ZeroDivisionError("Ошибка! Деление на ноль.\n")
+                stack.append(a // b)
+    print(stack[0])
+
+
+if __name__ == "__main__":
+    try:
+        expression = input()
+        calculator(validate_expression(expression))
+    except InputError as e:
+        print(e)
+    except ZeroDivisionError as e:
+        print(e)
